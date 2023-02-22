82b436103f0f540017f9e12a3966ea99201927e7
BM Peter <phuongtran@brickmate.vn>
2023-02-22 17:53:36 +0700

    Remove Item

--- src/home/header/index.tsx
+++ src/home/header/index.tsx
@@ -10,64 +10,6 @@ import Inputs from '../../elements/Input/index'
 import { connect } from 'react-redux';
 import { useEffect, useState } from 'react';
 const Header = (props: any) => {
-    const navigate = useNavigate();
-    const schema = yup.object().shape({
-        email: yup
-            .string()
-            .email("Vui lòng nhập đúng định dạng email.")
-            .required("Vui lòng nhập email."),
-        password: yup
-            .string()
-            .required("Vui lòng nhập password.")
-            .matches(
-                /^(?=\D*\d)(?=[^a-z]*[a-z])(?=.*[$@$!%*?&])(?=[^A-Z]*[A-Z]).{8,30}$/,
-                "Vui lòng nhập đúng định dạng password."
-            ),
-    });
-
-    const form = useForm({
-        defaultValues: { email: 'admin2@test.com', password: 'Abcd1234@' },
-        mode: "onTouched",
-        resolver: yupResolver(schema),
-    });
-    
-    const {
-        register,
-        control,
-        handleSubmit,
-        formState: { errors },
-        setError,
-        getValues
-    } = form;
-
-    const token = (dataToken?: any) => {
-        return {
-            type: "saveToken",
-            data: dataToken
-        };
-    }
-    const [dataToken, setdataToken] = useState<any>({})
-
-    useEffect(() => {})
-
-    const fetchData = async (data:any) => {
-        try {
-            const response = await http.post(`/login`, data)
-            setdataToken(response)
-            props.dispatch(token(response))
-            navigate('/home')
-        } catch (error: any) {
-            setError(error.error_field, {'message': error?.message})
-        }
-    }
-
-    const handleSubmitForm = (data: any) => {
-        fetchData(data);
-    }
-
-    const changeData = (data: any) => {
-        console.log('data', data);
-    }
 
     return (
         <div className={styleLogin['layer-item']}>
